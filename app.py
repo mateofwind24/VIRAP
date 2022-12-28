cb82c79512e9b2d199a73f8768410b2c07161ab5
HYUKJU NA <mate_of_wind@naver.com>
2022-12-29 02:43:47 +0900

    구동가능버전1

--- app.py
+++ app.py
@@ -7,100 +7,134 @@ app = Flask(__name__)
 def main():
     return "Hello VIRAP!"
 
-@app.route('/module4_unchange_param_input', methods=['POST'])
-def module4_unchange_param_input():
-    input1 = request.get_json()     # postman no list just dic
-    global row
-    global column
-    global totalnum
-    global chem     #plus 1~283 number
-    global S
-    global Hc
-    global Hr       #plus
-    global Hs       #plus
-    global Dair
-    global Dwater
-    global DHvb
-    global Tc
-    global Tb
-    global MW
-    global IUR
-    global Rfc
-    global Mut      # No=0, Yes=1, VC=2
-    global n        #cal
-    global Ls
-    global Ts
-    global Cs       # grid cal plus
-    global Cmedium  # grid
-    global WT       # grid
-    global LE       # grid
-    global foc      # grid
-    global Organic  # No=0 Yes=1
-    global Koc
-    global kd
-    global pH
-    global hSA      #cal
-    global nSA
-    global nwSA
-    global nairSA
-    global rhoSA
-    global hcz
-    global ncz
-    global nwcz
-    global naircz
-    global Lb
-    global Lf
-    global eta
-    global Abf
-    global Hb
-    global ach
-    global Qsoil_Qb
-    global Qb
-    global Qsoil
-    global Ex
-    global ATc      #cal
-    global ATnc     #cal
-    global MMOAF    #cal
-    global EF       #cal
-    global ED       #cal
-    global ET       #cal
-    global Cia      #plus
-    column = input1['column']
-    row = input1['row']
+@app.route('/module4', methods=['POST'])
+def module4():
+    inputdata = request.get_json()     # json no list just dic
+    '''
+    row
+    column
+    totalnum
+    chem        # 1~283 number
+    S
+    Hc
+    Hr          # cal
+    Hs          # cal
+    Dair
+    Dwater
+    DHvb
+    Tc
+    Tb
+    MW
+    IUR
+    Rfc
+    Mut         # No=0, Yes=1, VC=2
+    n           # cal
+    Ls
+    Ts
+    Cs          # grid cal
+    Cmedium     # gridinput
+    WT          # gridinput
+    LE          # gridinput
+    foc         # gridinput
+    Organic     # No=0 Yes=1
+    Koc
+    kd
+    pH
+    hSA         #cal
+    nSA
+    nwSA
+    nairSA
+    rhoSA
+    hcz
+    ncz
+    nwcz
+    naircz
+    Lb          # gridinput
+    Lf          # gridinput
+    eta         # gridinput
+    Abf         # gridinput
+    Hb          # gridinput
+    ach         # gridinput
+    Qsoil_Qb    # gridinput
+    Qb          # cal
+    Qsoil       # cal
+    Ex          # gridinput
+    ATc         # cal
+    ATnc        # cal
+    MMOAF       # cal
+    EF          # cal
+    ED          # cal
+    ET          # cal
+    Cia         # cal
+    '''
+    column = inputdata['column']
+    row = inputdata['row']
     totalnum = column*row
-    chem = input1['chem']
-    S = input1['S']
-    Hc = input1['Hc']
-    Dair = input1['Dair']
-    Dwater = input1['Dwater']
-    DHvb = input1['DHvb']
-    Tc = input1['Tc']
-    Tb = input1['Tb']
-    MW = input1['MW']
-    IUR = input1['IUR']
-    Rfc = input1['Rfc']
-    Mut = input1['Mut']
-    Ts = input1['Ts']
-    Organic = input1['Organic']
-    Koc = input1['Koc']
-    kd = input1['kd']
-    pH = input1['pH']
-    nSA = input1['nSA']
-    nwSA = input1['nwSA']
-    nairSA = input1['nairSA']
-    rhoSA = input1['rhoSA']
-    hcz = input1['hcz']     #cm
-    ncz = input1['ncz']
-    nwcz = input1['nwcz']
-    naircz = input1['naircz']
-    Lb = input1['Lb']
-    Lf = input1['Lf']
-    eta = input1['eta']
-    Abf = input1['Abf']
-    Hb = input1['Hb']
-    ach = input1['ach']
-    Qsoil_Qb = input1['Qsoil_Qb']
-    Ex = input1['Ex']
+    chem = inputdata['chem']
+    S = inputdata['S']
+    Hc = inputdata['Hc']
+    Dair = inputdata['Dair']
+    Dwater = inputdata['Dwater']
+    DHvb = inputdata['DHvb']
+    Tc = inputdata['Tc']
+    Tb = inputdata['Tb']
+    MW = inputdata['MW']
+    IUR = inputdata['IUR']
+    Rfc = inputdata['Rfc']
+    Mut = inputdata['Mut']
+    Ts = inputdata['Ts']
+    Organic = inputdata['Organic']
+    Koc = inputdata['Koc']
+    kd = inputdata['kd']
+    pH = inputdata['pH']
+    nSA = inputdata['nSA']
+    nwSA = inputdata['nwSA']
+    nairSA = inputdata['nairSA']
+    rhoSA = inputdata['rhoSA']
+    hcz = inputdata['hcz']     #cm
+    ncz = inputdata['ncz']
+    nwcz = inputdata['nwcz']
+    naircz = inputdata['naircz']
+    # grid input start
+    Cmedium_input = inputdata['Cmedium']
+    WT_input = inputdata['WT']
+    LE_input = inputdata['LE']
+    foc_input = inputdata['foc']
+    Lb_input = inputdata['Lb']
+    Lf_input = inputdata['Lf']
+    eta_input = inputdata['eta']
+    Abf_input = inputdata['Abf']
+    Hb_input = inputdata['Hb']
+    ach_input = inputdata['ach']
+    Qsoil_Qb_input = inputdata['Qsoil_Qb']
+    Ex_input = inputdata['Ex']
+    Cmedium = [[0 for j in range(row)] for i in range(column)]
+    WT = [[0 for j in range(row)] for i in range(column)]
+    LE = [[0 for j in range(row)] for i in range(column)]
+    foc = [[0 for j in range(row)] for i in range(column)]
+    Lb = [[0 for j in range(row)] for i in range(column)]
+    Lf = [[0 for j in range(row)] for i in range(column)]
+    eta = [[0 for j in range(row)] for i in range(column)]
+    Abf = [[0 for j in range(row)] for i in range(column)]
+    Hb = [[0 for j in range(row)] for i in range(column)]
+    ach = [[0 for j in range(row)] for i in range(column)]
+    Qsoil_Qb = [[0 for j in range(row)] for i in range(column)]
+    Ex = [[0 for j in range(row)] for i in range(column)]
+    for i in range(column):
+        for j in range(row):
+            Cmedium[i][j] = float(Cmedium_input[i][j])
+            LE[i][j] = float(LE_input[i][j])
+            WT[i][j] = float(WT_input[i][j])
+            foc[i][j] = float(foc_input[i][j])
+            Lb[i][j] = float(Lb_input[i][j])
+            Lf[i][j] = float(Lf_input[i][j])
+            eta[i][j] = float(eta_input[i][j])
+            Abf[i][j] = float(Abf_input[i][j])
+            Hb[i][j] = float(Hb_input[i][j])
+            ach[i][j] = float(ach_input[i][j])
+            Qsoil_Qb[i][j] = float(Qsoil_Qb_input[i][j])
+            Ex[i][j] = int(Ex_input[i][j])
+    # constant, no grid param
     ATc = 70
     MMOAF = 72
     if Ex == 0:
@@ -124,195 +158,118 @@ def module4_unchange_param_input():
     else:
         n = 0.41
     Hr = Hc/(0.000082057*298)
-    Qb = Abf*Hb*ach
-    Qsoil = Qsoil_Qb*Qb
     DHvs = DHvb*(math.pow((1-Ts/Tc)/(1-Tb/Tc),n))
     Hs = (math.exp(-(DHvs/Rc)*(1/Ts-1/Tr))*Hc)/(R*Ts)
+    # grid param cal
+    Qb = [[0 for j in range(row)] for i in range(column)]
+    Qsoil = [[0 for j in range(row)] for i in range(column)]
     Cs = [[0 for j in range(row)] for i in range(column)]
     Ls = [[0 for j in range(row)] for i in range(column)]
     hSA = [[0 for j in range(row)] for i in range(column)]
     for i in range(column):
         for j in range(row):
+            Qb[i][j] = Abf[i][j]*Hb[i][j]*ach[i][j]
+            Qsoil[i][j] = Qsoil_Qb[i][j]*Qb[i][j]
             Cs[i][j] = Hs*Cmedium[i][j]*1000
             Ls[i][j] = LE[i][j]-WT[i][j]
             hSA[i][j] = Ls[i][j]
-
-@app.route('/module4_Cmedium_input', methods=['POST'])
-def module4_Cmedium_input():
-    global Cmedium
-    global column
-    global row
-    Cmedium = [[0 for j in range(row)] for i in range(column)]
-    input_Cmedium = request.get_json()
+    # VFwesp calculate CM6a(Qs>0)
+    DeffT_6a = [[0 for j in range(row)] for i in range(column)]
+    A_param_6a = [[0 for j in range(row)] for i in range(column)]
+    B_param_6a = [[0 for j in range(row)] for i in range(column)]
+    C_param_6a = [[0 for j in range(row)] for i in range(column)]
+    VFwesp_6a = [[0 for j in range(row)] for i in range(column)]
+    DeffA_6a = Dair*(math.pow((nSA-nwSA),3.33)/math.pow(nSA,2))+(Dwater/Hs)*(math.pow(nwSA,3.33)/math.pow(nSA,2))
+    DeffCZ_6a = Dair*(math.pow((ncz-nwcz),3.33)/math.pow(ncz,2))+(Dwater/Hs)*(math.pow(nwcz,3.33)/math.pow(ncz,2))
     for i in range(column):
         for j in range(row):
-            Cmedium[i][j] = float(input_Cmedium[i][j])
-    return Cmedium
-
-@app.route('/module4_WT_input', methods=['POST'])
-def module4_WT_input():
-    global WT
-    global column
-    global row
-    WT = [[0 for j in range(row)] for i in range(column)]
-    input_WT = request.get_json()
-    for i in range(column):
-        for j in range(row):
-            WT[i][j] = float(input_WT[i][j])
-    return WT
-
-@app.route('/module4_LE_input', methods=['POST'])
-def module4_LE_input():
-    global LE
-    global column
-    global row
-    LE = [[0 for j in range(row)] for i in range(column)]
-    input_LE = request.get_json()
-    for i in range(column):
-        for j in range(row):
-            LE[i][j] = float(input_LE[i][j])
-    return LE
-
-@app.route('/module4_foc_input', methods=['POST'])
-def module4_foc_input():
-    global foc
-    global column
-    global row
-    foc = [[0 for j in range(row)] for i in range(column)]
-    input_foc = request.get_json()
-    for i in range(column):
-        for j in range(row):
-            foc[i][j] = float(input_foc[i][j])
-    return foc
-
-@app.route('/module4_totalcalculate')
-def module4_totalcalculate():
-    global row
-    global column
-    global totalnum
-    global chem     #plus 1~283 number
-    global S
-    global Hc
-    global Hr       #plus
-    global Hs       #plus
-    global Dair
-    global Dwater
-    global DHvb
-    global Tc
-    global Tb
-    global MW
-    global IUR
-    global Rfc
-    global Mut      # No=0, Yes=1, VC=2
-    global n        #cal
-    global Ls
-    global Ts
-    global Cs       # grid cal plus
-    global Cmedium  # grid
-    global WT       # grid
-    global LE       # grid
-    global foc      # grid
-    global Organic  # No=0 Yes=1
-    global Koc
-    global kd
-    global pH
-    global hSA      #cal
-    global nSA
-    global nwSA
-    global nairSA
-    global rhoSA
-    global hcz
-    global ncz
-    global nwcz
-    global naircz
-    global Lb
-    global Lf
-    global eta
-    global Abf
-    global Hb
-    global ach
-    global Qsoil_Qb
-    global Qb
-    global Qsoil
-    global Ex
-    global ATc      #cal
-    global ATnc
-    global MMOAF    #cal
-    global EF       #cal
-    global ED       #cal
-    global ET       #cal
-    global Cia      #plus
-    data = [[0 for j in range(10)] for i in range(1)]
-    for i in range(1):
-        # VFwesp calculate CM6a(Qs>0) loop
-        DeffA_6a = Dair*(math.pow((nSA-nwSA),3.33)/math.pow(nSA,2))+(Dwater/Hs)*(math.pow(nwSA,3.33)/math.pow(nSA,2))
-        DeffCZ_6a = Dair*(math.pow((ncz-nwcz),3.33)/math.pow(ncz,2))+(Dwater/Hs)*(math.pow(nwcz,3.33)/math.pow(ncz,2))
-        DeffT_6a = (hSA-Lb)/((hSA-Lb-hcz)/DeffA_6a+hcz/DeffCZ_6a)
+            DeffT_6a[i][j] = (hSA[i][j]-Lb[i][j])/((hSA[i][j]-Lb[i][j]-hcz)/DeffA_6a+hcz/DeffCZ_6a)
         if Qsoil == 0:
-            A_param_6a = (DeffT_6a*(Abf+4*Lb*math.sqrt(Abf)*0.36))/(Qb*(Ls-Lb))
-            VFwesp_6a = A_param_6a/(1+A_param_6a+((DeffT_6a*Lf)/((Ls-Lb)*DeffA_6a*eta)))
+            A_param_6a[i][j] = (DeffT_6a[i][j]*(Abf[i][j]+4*Lb[i][j]*math.sqrt(Abf[i][j])*0.36))/(Qb[i][j]*(Ls[i][j]-Lb[i][j]))
+            VFwesp_6a[i][j] = A_param_6a[i][j]/(1+A_param_6a[i][j]+((DeffT_6a[i][j]*Lf[i][j])/((Ls[i][j]-Lb[i][j])*DeffA_6a*eta[i][j])))
         else:
-            A_param_6a = (DeffT_6a*(Abf+4*Lb*math.sqrt(Abf)*0.36))/(Qb*(Ls-Lb))
-            B_param_6a = (Qsoil_Qb*Qb*Lf)/(DeffA_6a*eta*(Abf+4*Lb*math.sqrt(Abf))*0.36)
-            C_param_6a = Qsoil_Qb
-            VFwesp_6a = (A_param_6a*(math.exp(B_param_6a)))/(math.exp(B_param_6a)+A_param_6a+(A_param_6a/C_param_6a)*(math.exp(B_param_6a)-1))
-        # VFsesp calculate CM4 loop
-        if Organic == 0:
-            ks = kd
-        else:
-            ks = Koc*foc    #loop
-        DeffA_4a = Dair*(math.pow((nSA-nwSA),3.33)/math.pow(nSA,2))+(Dwater/Hs)*(math.pow(nwSA,3.33)/math.pow(nSA,2))
-        DeffCZ_4a = Dair*(math.pow((ncz-nwcz),3.33)/math.pow(ncz,2))+(Dwater/Hs)*(math.pow(nwcz,3.33)/math.pow(ncz,2))  # my think
-        DeffT_4a = (hSA-Lb)/((hSA-Lb-hcz)/DeffA_4a+hcz/DeffCZ_4a)   #my think
-        A_param_4a = (Hs*rhoSA)/(nwSA+ks*rhoSA+Hs*nairSA)
-        B_param_4a = (Qsoil_Qb*Qb*Lf)/(DeffA_4a*eta*(Abf+4*Lb*math.sqrt(Abf))*0.36)
-        C_param_4a = (DeffA_4a*(Abf+4*Lb*math.sqrt(Abf))*0.36)/(Qb*Ls)
-        if Qsoil == 0:
-            VFsesp_4a = (A_param_4a*C_param_4a)/(1+A_param_4a+((DeffT_4a*Lf)/Ls*DeffA_4a*eta))
-        else:
-            VFsesp_4a = (A_param_4a*C_param_4a*math.exp(B_param_4a))/(math.exp(B_param_4a)+C_param_4a+(A_param_4a/C_param_4a)*(math.exp(B_param_4a)-1))
-        # Risk calculate loop
-        mIURTCE_R_GW = 1.0e-6;
-        IURTCE_R_GW = 3.1e-6;
-        mIURTCE_C_GW = 4.1e-6;
-        IURTCE_C_GW = 4.1e-6;
-        Cia = VFwesp_6a*Cs  #loop
-        if chem == 258 and Ex == 0:
-            Risk = (Cia*mIURTCE_R_GW*MMOAF*EF*ET)/(ATc*365*24) + (Cia*IURTCE_R_GW*ED*EF*ET)/(ATc*365*24)
-        elif chem == 258 and Ex == 1:
-            Risk = (Cia*mIURTCE_C_GW*MMOAF*EF*ET)/(ATc*365*24) + (Cia*IURTCE_C_GW*ED*EF*ET)/(ATc*365*24)
-        elif Mut == 0 and Ex == 0:
-            Risk = (IUR*EF*ED*ET*Cia)/(ATc*365*24)
-        elif Mut == 0 and Ex == 1:
-            Risk = (IUR*EF*ED*ET*Cia)/(ATc*365*24)
-        elif Mut == 1 and chem != 258:                  # check no Ex
-            Risk = (IUR*EF*MMOAF*ET*Cia)/(ATc*365*24)
-        elif Mut == 2 and Ex == 0:
-            Risk = Cia*(IUR+(IUR*ED*EF*ET)/(ATc*365*24))
-        elif Mut == 2 and Ex == 1:
-            Risk = Cia*(IUR*ED*EF*ET)/(ATc*365*24)
-        # HQ calculate loop
-            HQ = (EF*ED*(ET/24)*Cia)/(Rfc*1000*ATnc*365)
-        # Cca, Cnca calculate
-        mIURTCE_R = 1
-        IURTCE_R = 1
-        TCR = 1.0e-6
-        THQ = 1
-        if chem == 258:
-            Cca = 1/((EF*MMOAF*ET*mIURTCE_R/(TCR*ATc*365*24))+EF*ED*ET*IURTCE_R/(TCR*ATc*365*24))
-            Cnca = 1/((EF*MMOAF*ET*mIURTCE_R/(TCR*ATc*365*24))+EF*ED*ET*IURTCE_R/(TCR*ATc*365*24))
-        else:
+            A_param_6a[i][j] = (DeffT_6a[i][j]*(Abf[i][j]+4*Lb[i][j]*math.sqrt(Abf[i][j])*0.36))/(Qb[i][j]*(Ls[i][j]-Lb[i][j]))
+            B_param_6a[i][j] = (Qsoil_Qb[i][j]*Qb[i][j]*Lf[i][j])/(DeffA_6a*eta[i][j]*(Abf[i][j]+4*Lb[i][j]*math.sqrt(Abf[i][j]))*0.36)
+            C_param_6a[i][j] = Qsoil_Qb[i][j]
+            VFwesp_6a[i][j] = (A_param_6a[i][j]*(math.exp(B_param_6a[i][j])))/(math.exp(B_param_6a[i][j])+A_param_6a[i][j]+(A_param_6a[i][j]/C_param_6a[i][j])*(math.exp(B_param_6a[i][j])-1))
+    # VFsesp calculate CM4
+    ks = [[0 for j in range(row)] for i in range(column)]
+    DeffT_4a = [[0 for j in range(row)] for i in range(column)]
+    A_param_4a = [[0 for j in range(row)] for i in range(column)]
+    B_param_4a = [[0 for j in range(row)] for i in range(column)]
+    C_param_4a = [[0 for j in range(row)] for i in range(column)]
+    VFsesp_4a = [[0 for j in range(row)] for i in range(column)]
+    DeffA_4a = Dair*(math.pow((nSA-nwSA),3.33)/math.pow(nSA,2))+(Dwater/Hs)*(math.pow(nwSA,3.33)/math.pow(nSA,2))
+    DeffCZ_4a = Dair*(math.pow((ncz-nwcz),3.33)/math.pow(ncz,2))+(Dwater/Hs)*(math.pow(nwcz,3.33)/math.pow(ncz,2))
+    for i in range(column):
+        for j in range(row):
+            if Organic == 0:
+                ks[i][j] = kd
+            else:
+                ks[i][j] = Koc*foc[i][j]
+            DeffT_4a[i][j] = (hSA[i][j]-Lb[i][j])/((hSA[i][j]-Lb[i][j]-hcz)/DeffA_4a+hcz/DeffCZ_4a)
+            A_param_4a[i][j] = (Hs*rhoSA)/(nwSA+ks[i][j]*rhoSA+Hs*nairSA)
+            B_param_4a[i][j] = (Qsoil_Qb[i][j]*Qb[i][j]*Lf[i][j])/(DeffA_4a*eta[i][j]*(Abf[i][j]+4*Lb[i][j]*math.sqrt(Abf[i][j]))*0.36)
+            C_param_4a[i][j] = (DeffA_4a*(Abf[i][j]+4*Lb[i][j]*math.sqrt(Abf[i][j]))*0.36)/(Qb[i][j]*Ls[i][j])
+            if Qsoil == 0:
+                VFsesp_4a[i][j] = (A_param_4a[i][j]*C_param_4a[i][j])/(1+A_param_4a[i][j]+((DeffT_4a[i][j]*Lf[i][j])/Ls[i][j]*DeffA_4a[i][j]*eta[i][j]))
+            else:
+                VFsesp_4a[i][j] = (A_param_4a[i][j]*C_param_4a[i][j]*math.exp(B_param_4a[i][j]))/(math.exp(B_param_4a[i][j])+C_param_4a[i][j]+(A_param_4a[i][j]/C_param_4a[i][j])*(math.exp(B_param_4a[i][j])-1))
+    # Risk calculate loop
+    mIURTCE_R_GW = 1.0e-6;
+    IURTCE_R_GW = 3.1e-6;
+    mIURTCE_C_GW = 4.1e-6;
+    IURTCE_C_GW = 4.1e-6;
+    Cia = [[0 for j in range(row)] for i in range(column)]
+    Risk = [[0 for j in range(row)] for i in range(column)]
+    for i in range(column):
+        for j in range(row):
+            Cia[i][j] = VFwesp_6a[i][j]*Cs[i][j]
+            if chem == 258 and Ex == 0:
+                Risk[i][j] = (Cia[i][j]*mIURTCE_R_GW*MMOAF*EF*ET)/(ATc*365*24) + (Cia[i][j]*IURTCE_R_GW*ED*EF*ET)/(ATc*365*24)
+            elif chem == 258 and Ex == 1:
+                Risk[i][j] = (Cia[i][j]*mIURTCE_C_GW*MMOAF*EF*ET)/(ATc*365*24) + (Cia[i][j]*IURTCE_C_GW*ED*EF*ET)/(ATc*365*24)
+            elif Mut == 0:                          # check no Ex
+                Risk[i][j] = (IUR*EF*ED*ET*Cia[i][j])/(ATc*365*24)
+            elif Mut == 1 and chem != 258:          # check no Ex
+                Risk[i][j] = (IUR*EF*MMOAF*ET*Cia[i][j])/(ATc*365*24)
+            elif Mut == 2 and Ex == 0:
+                Risk[i][j] = Cia[i][j]*(IUR+(IUR*ED*EF*ET)/(ATc*365*24))
+            elif Mut == 2 and Ex == 1:
+                Risk[i][j] = Cia[i][j]*(IUR*ED*EF*ET)/(ATc*365*24)
+    # HQ calculate
+    HQ = [[0 for j in range(row)] for i in range(column)]
+    for i in range(column):
+        for j in range(row):
+            if Rfc != 0:
+                HQ[i][j] = (EF*ED*(ET/24)*Cia[i][j])/(Rfc*1000*ATnc*365)
+            else:
+                HQ[i][j] = -9999
+    # Cca, Cnca calculate
+    mIURTCE_R = 1
+    IURTCE_R = 1
+    TCR = 1.0e-6
+    THQ = 1
+    if chem == 258:
+        Cca = 1/((EF*MMOAF*ET*mIURTCE_R/(TCR*ATc*365*24))+EF*ED*ET*IURTCE_R/(TCR*ATc*365*24))
+        Cnca = 1/((EF*MMOAF*ET*mIURTCE_R/(TCR*ATc*365*24))+EF*ED*ET*IURTCE_R/(TCR*ATc*365*24))
+    else:
+        if IUR != 0 and Rfc != 0:
             Cca = (TCR*ATc*365*24)/(EF*ED*ET*IUR)
             Cnca = (THQ*Rfc*ATnc*365*24*1000)/(EF*ED*ET)
-        data[i][0] = VFwesp_6a
-        data[i][1] = VFsesp_4a
-        data[i][2] = Risk
-        data[i][3] = HQ
-        data[i][4] = Cca
-        data[i][5] = Cnca
-        data[i][6] = Cia
+        else:
+            Cca = -9999
+            Cnca = -9999
+    data = {
+        "chem": chem,
+        "VFwesp": VFwesp_6a,
+        "VFsesp": VFsesp_4a,
+        "Risk": Risk,
+        "HQ": HQ,
+        "Cca": Cca,
+        "Cnca": Cnca
+        }
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
